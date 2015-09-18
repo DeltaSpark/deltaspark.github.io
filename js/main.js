d31fc5a629b20313f0225755ff9ceda6c8e639f8
Andy Chu <vox@andychu.me>
2015-09-17 23:51:24 -0700

    feat: Add smooth scrolling and correct label color

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ $(document).ready(function () {
     $(".navbar-nav li a").click(function(event) {
         $(".navbar-collapse").collapse('hide');
     });
+    
+    $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
 
 $(function(){
