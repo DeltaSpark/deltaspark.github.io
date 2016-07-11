4a6d2cb92a1e43ea84515231acde2de2e39b599f
Andy Chu <andy@andychu.me>
2016-07-11 00:12:33 -0700

    Add workplace expandable details, and analytics
    Added workplace with expandable details:
     - Customized grid.js to accept additional details including subtitle
     - Changed grid.js to inline margin at bottom
    
    Added Google Analytics:
     - Uses staging GA property
     - Deployment Task: Switch to production GA
     - Added click events to all converting links

--- js/grid.js
+++ js/grid.js
@@ -343,9 +343,10 @@ var Grid = (function() {
 		create : function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
-			this.$description = $( '<p></p>' );
+			this.$subtitle = $( '<h4 style="margin-bottom: 10px;"></h4>' );
+			this.$description = $( '<p style="margin-bottom: 5px;"></p>' );
 			this.$href = $( '<a href="#">Visit website</a>' );
-			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
+			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$subtitle, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
 			this.$closePreview = $( '<span class="og-close"></span>' );
@@ -382,10 +383,12 @@ var Grid = (function() {
 					href : $itemEl.attr( 'href' ),
 					largesrc : $itemEl.data( 'largesrc' ),
 					title : $itemEl.data( 'title' ),
+					subtitle : $itemEl.data( 'subtitle' ),
 					description : $itemEl.data( 'description' )
 				};
 
 			this.$title.html( eldata.title );
+            this.$subtitle.html( eldata.subtitle );
 			this.$description.html( eldata.description );
 			this.$href.attr( 'href', eldata.href );
 
